7346b3616291638b76c63b2da48875f66c610a31
SteveJiangMutao <yujiangzjut@163.com>
2026-02-01 01:25:58 +0800

    update

--- netlify/functions/hello.js
+++
@@ -1,7 +0,0 @@
-// Simple Netlify function to ensure Node.js environment
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Netlify Function" })
-  };
-}
